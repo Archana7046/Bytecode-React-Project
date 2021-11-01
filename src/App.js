812a8e475748b207669c5ce9231a2e03d54f1905
Archana7046 <84974842+Archana7046@users.noreply.github.com>
2021-11-01 17:00:07 +0530

    Batches Section

---
+++ src/App.js
@@ -0,0 +1,30 @@
+//import React from 'react'
+
+/*import React from "react";
+
+const App = () => {
+  return(
+  <>
+  <FirstName/>
+  <h1 className="myclassname"> hello world  </h1>
+  <p>Awesome</p>
+  <img src="" alt ="" />
+  </>
+  );
+}
+
+const FirstName = () =>{
+  return <h1> Bytecode clsses</h1>
+}
+export default App;
+*/
+
+import React from "react";
+import Batches from "./component/Basics/Batches";
+
+
+const App = () => {
+  return <Batches/>; 
+};
+
+export default App;
---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,268 @@
+const Menu = [
+    {
+      id: 1,
+      image: "../images/umesh.jpeg",
+      name: "Umesh Kumari",
+      category: "2020",
+      price: "12₹",
+      description:
+        "Benefits of Bytecode community :  Beginner friendly,learn coding by projects ,introduction to latest technology, boost your confidence ,presentation skills and team management",
+    },
+  
+    {
+      id: 2,
+      image: "../images/archana.jpeg",
+      name: "Archana Kumari",
+      category: "2020",
+      price: "20₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    
+    {
+      id: 3,
+      image: "images/susmita.jpeg",
+      name: "Susmita Sagar",
+      category: "2020",
+      price: "10₹",
+      description:
+        "Bytecode coding club is more about getting like-minded students together to talk about and perhaps work on coding projects together. Effective learning is possible with the help of seniors. For me, Bytecode was the first and best initiative for Coading. ",
+    },
+    {
+      id: 4,
+      image: "../images/ashish.jpeg",
+      name: "Ashish Sinha",
+      category: "2020",
+      price: "50₹",
+      description:
+        "Bytecode is a club in which we all are  learnt coding and many more skills like interaction between each others . In this club our mentor is our senior so they treat us as like  a  friend. They are many projects to do so you all are explore many new technologies and skills.",
+    },
+    {
+      id: 5,
+      image: "../images/Ayush.jpeg",
+      name: "Ayush Kumar",
+      category: "2020",
+      price: "80₹",
+      description:
+        "Bytecodelearners.club It is one of The very Good Coding Clubs I have joined So far. Have Also Get to know about others Coding Clubs Of Friends but they r Not very Much Supportive like Bytecode is I would call it a Influncer Club That Always Supports and also Orgazises Very Good Session as well.So I would definitely Recommend my Juniors to Join this. Thank You 😊",
+    },
+    {
+      id: 6,
+      image: "../images/birender.jpeg",
+      name: "Birender Bawlia",
+      category: "2020",
+      price: "180₹",
+      description:
+        "Bytecodelearners is among one of the best coding clubs. Our mentors are very helpful and always act like our friends. We get many chances here to work in team and explore our leadership and team-management skills too. If you are willing to learn coding then Bytecodelearners is perfect place for you ❤️",
+    },
+    {
+      id: 7,
+      image: "../images/hari.jpeg",
+      name: "Hari Kante",
+      category: "2020",
+      price: "60₹",
+      description:
+        "For me, the Bytecode club is all about friends. I haven't gone to college yet, still I have found friends who are more like me with similar interests. In this club, we get to do some cool group projects where we help each other to accomplish them. The best part is, here you need to work beyond your limits which actually helps you to improve your skills.",
+    },
+    {
+      id: 8,
+      image: "../images/sai.jpeg",
+      name: "Sai Kumar Bandla",
+      category: "2020",
+      price: "60₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../images/shailender.jpeg",
+      name: "Shailender",
+      category: "2020",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 9,
+      image: "../images/dipu-kumar-sah.jpg",
+      name: "Dipu Kumar Sah",
+      category: "2018",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 10,
+      image: "../images/yash-raj.jpg",
+      name: "Yash Raj",
+      category: "2018",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 11,
+      image: "../images/raj-patel.jpg",
+      name: "Raj Patel",
+      category: "2018",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 12,
+      image: "../images/shantanu-pratap.jpg",
+      name: "Shantanu Pratap",
+      category: "2018",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 13,
+      image: "../images/sourabh saraswat.jpeg",
+      name: "Sourabh Saraswat",
+      category: "2019",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 14,
+      image: "../images/abhishek rao.jpeg",
+      name: "Abhishek Rao",
+      category: "2019",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 15,
+      image: "../images/sourav sharma.jpeg",
+      name: "Sourav Sharma",
+      category: "2019",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 16,
+      image: "../images/jitendra kumar.jpeg",
+      name: "Jitendra Kumar",
+      category: "2019",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },{
+      id: 17,
+      image: "../images/abhishek tiwari.jpeg",
+      name: "Abhishek Tiwari",
+      category: "2019",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },{
+      id: 18,
+      image: "../images/ritika sinha.jpeg",
+      name: "Ritika Sinha",
+      category: "2019",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },{
+      id: 19,
+      image: "../images/yamini khurana.jpeg",
+      name: "Yamini Khurana",
+      category: "2019",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 20,
+      image: "../images/dewang sharma.jpeg",
+      name: "Dewang Sharma",
+      category: "2019",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 21,
+      image: "../images/deepak garg.jpeg",
+      name: "Deepak Garg",
+      category: "2019",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 22,
+      image: "../images/joginder sharma.jpeg",
+      name: "Joginder Sharma",
+      category: "2019",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 23,
+      image: "../images/samosa.jpg",
+      name: "samosa",
+      category: "upcoming",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 24,
+      image: "../images/samosa.jpg",
+      name: "samosa",
+      category: "upcoming",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 25,
+      image: "../images/samosa.jpg",
+      name: "samosa",
+      category: "upcoming",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 26,
+      image: "../images/samosa.jpg",
+      name: "samosa",
+      category: "upcoming",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 27,
+      image: "../images/samosa.jpg",
+      name: "samosa",
+      category: "upcoming",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 28,
+      image: "../images/samosa.jpg",
+      name: "samosa",
+      category: "upcoming",
+      price: "10₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    
+  ];
+  
+  export default Menu;
